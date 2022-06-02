cefbb0f28451cb1bd1e0787930eada7c2086c1ce
Agustin <agsutinromano10@yahoo.com>
2022-06-02 14:41:10 -0400

    Styling is done working on JS.

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberBtns = document.querySelectorAll("[data-number]");
+const operationBtns = document.querySelectorAll("[data-operation]");
+const equalBtn = document.querySelector("[data-equal]");
+const clearBtn = document.querySelector("[data-clear]");
+const deletBtn = document.querySelector("[data-delete]");
+const prevOutput = document.querySelector("[data-prev-output]");
+const output = document.querySelector("[data-output]");
